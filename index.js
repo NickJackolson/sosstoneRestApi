024b64008d68c207de44fb3742fa6c12dd3d6be9
NickJackolson <metedurlu@gmail.com>
2021-04-24 21:25:17 +0300

    fixed routes

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ const app = Express();
 const port = 9000;
 app.use(Express.json())
 
-// some sort of data structure to hold input strings with counts
+// data structure to hold input strings with counts
 var myStructure = {};
 
 // TODO POST request at /input
 app.post("/input", (req,res) =>{
     
     for (const [_, value] of Object.entries(req.body)) {
-        if (value in myStructure){
+        if (value in myStructure)
             myStructure[value] += 1;
-        }else{
+        else
             myStructure[value] = 1;
-        }
     }
     res.send("Input received!");
+
 });
 
 // TODO GET request at /<query>
-app.get("/", (req,res) => {
-    if(myStructure[req.query.query] == undefined)
-    res.send("0");
+app.get("/query", (req,res) => {
+    if (myStructure[req.query.key] == undefined)
+        res.send("0");
     else
-    res.json(myStructure[req.query.query]);
+        res.json(myStructure[req.query.key]);
+
+});
+
+// List all
+app.get("/", (req,res) => {
+    res.json(myStructure);
 });
 
 
