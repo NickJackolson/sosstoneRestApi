144aeea0f17d9557a6601bdbff2b401f89a44357
NickJackolson <metedurlu@gmail.com>
2021-04-24 21:07:01 +0300

    added GET route

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ const app = Express();
 const port = 9000;
 app.use(Express.json())
 
-// some sort of data structure to hold input strings with keys
+// some sort of data structure to hold input strings with counts
 var myStructure = {};
 
-// TODO Listen at 9000
 // TODO POST request at /input
 app.post("/input", (req,res) =>{
-
+    
     for (const [_, value] of Object.entries(req.body)) {
-       if (value in myStructure){
-           myStructure[value] += 1;
-       }else{
-           myStructure[value] = 1;
-       }
+        if (value in myStructure){
+            myStructure[value] += 1;
+        }else{
+            myStructure[value] = 1;
+        }
     }
     res.send("Input received!");
-    
 });
+
 // TODO GET request at /<query>
 app.get("/", (req,res) => {
-    if(req.query)
-    res.send(myStructure);
+    if(myStructure[req.query.query] == undefined)
+    res.send("0");
+    else
+    res.json(myStructure[req.query.query]);
 });
 
 
+// TODO Listen at 9000
 app.listen(port, ()=>{
     console.log("Server started on port " + port);
 })
