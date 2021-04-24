65e0a3f29347a95a521181989ab1d5b6760d484a
NickJackolson <metedurlu@gmail.com>
2021-04-24 19:56:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import Express from "express";
+
+const app = Express();
+
+// TODO Listen at 9000
+// TODO POST request at /input
+// some sort of data structure to hold input strings
+// TODO GET request at /<query>
